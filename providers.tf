91b01b7492758797248f1ae80d6a4e9b4c37b365
Given <givencingco@gmail.com>
2024-10-23 13:31:32 +0200

    Initial commit

---
+++ providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket         = "given-cingco-devops-directive-tf-state"
+    key            = "Elastic_Beanstalk_Terraform/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
